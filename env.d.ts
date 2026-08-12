60bd728f95a8e9ff3a64618ad09a50c8d21963ad
m-fahimipour <mostafa.fahimipour@gmail.com>
2026-08-12 17:43:40 +0330

    feat: implement env config

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production';
+    SERVER_PORT: number;
+    DB_HOST: string;
+    DB_PORT: number;
+    DB_USERNAME: string;
+    DB_PASSWORD: string;
+    DB_NAME: string;
+  }
+}
